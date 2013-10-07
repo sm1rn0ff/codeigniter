1e7d469c6a929ba69ed16b52ee788bb7ab1f4f71
sm1rn0ff <garichard01@gmail.com>
2013-10-07 08:12:21 +0200

    changing htaccess

--- application/views/vue.php
+++ application/views/vue.php
@@ -12,7 +12,7 @@
         <![endif]-->
         <!--[if lt IE 9]>
             <link rel="stylesheet" href="style_ie_9.css" />
-            <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+            
         <![endif]-->
     </head>
     <body>
@@ -21,7 +21,7 @@
             <div id="main-content">
                 <section><?php echo $pseudo; ?></section>
                 <section><?php echo $email ?></section>
-                <section>section3</section>
+                <section><a href="<?php echo site_url('forum/bonjour') ?>">Bonjour</a></section>
             </div>
             <footer></footer>
         </div>
